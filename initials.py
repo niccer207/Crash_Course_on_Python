8898c7dc685037dd149b1c483f5834771454ab0b
Vlad Gusev <vladgusev1112@gmail.com>
2022-09-22 20:57:55 +0200

    added initials.py

---
+++ initials.py
@@ -0,0 +1,7 @@
+V             V   L               A           DDDDDDDDD
+ V           V    L              A A          D        D
+  V         V     L             A   A         D        D
+   V       V      L            A     A        D        D
+    V     V       L           AAAAAAAAA       D        D
+      V V         L          A         A      D        D
+       V          LLLLLL    A           A     DDDDDDDDDD
